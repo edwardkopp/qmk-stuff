60cc2a20f96b3a8677cb720d6b7fe74c1e3f8f01
Edward Kopp <edward@edwardkopp.com>
2022-08-25 00:39:13 -0400

    Update comment

--- keyboards/preonic/keymap.c
+++ keyboards/preonic/keymap.c
@@ -25,7 +25,7 @@ enum rainbow_layer_cake {
     RAT
 };
 
-// remove first row on each layer and it's literally a working planck
+// remove first row on each layer and it's literally the planck keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [TXT] = LAYOUT_preonic_grid(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    NK_ON,
