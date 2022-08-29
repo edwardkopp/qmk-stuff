9091b40ec3990c6e4208a3d437e5107255e70c8c
Edward Kopp <edward@edwardkopp.com>
2022-08-29 02:04:24 -0400

    Keymap revisions for Planck and Preonic

--- keyboards/preonic/keymap.c
+++ keyboards/preonic/keymap.c
@@ -38,22 +38,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ENT,
-        _______, XXXXXXX, KC_PIPE, KC_LCBR, KC_RCBR, KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, _______,
-        _______, _______, _______, _______, _______, KC_UNDS, KC_UNDS, _______, _______, _______, _______, _______
+        _______, KC_PIPE, KC_LCBR, KC_RCBR, KC_PLUS, KC_UNDS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
         ),
     [NAV] = LAYOUT_preonic_grid(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_PGUP, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_INS,
-        XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX, XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_PSCR,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
-        _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______
+        KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, GUI_OFF, KC_MPRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_DEL,
+        KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, GUI_ON,  KC_MPLY, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_ENT,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MNXT, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
         ),
     [RAT] = LAYOUT_preonic_grid(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        NK_OFF,  XXXXXXX, KC_MS_U, XXXXXXX, GUI_OFF, KC_MPRV, KC_VOLU, XXXXXXX, KC_WH_U, KC_BTN3, XXXXXXX, XXXXXXX,
-        NK_ON,   KC_MS_L, KC_MS_D, KC_MS_R, GUI_ON,  KC_MPLY, KC_MUTE, KC_BTN1, KC_WH_D, KC_BTN2, KC_ACL0, XXXXXXX,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MNXT, KC_VOLD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-        _______, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, XXXXXXX, _______
+        KC_INS,  KC_HOME, KC_MS_U, KC_END,  KC_PGUP, NK_OFF,  KC_VOLU, XXXXXXX, KC_WH_U, KC_BTN3, XXXXXXX, KC_DEL,
+        KC_PSCR, KC_MS_L, KC_MS_D, KC_MS_R, KC_PGDN, NK_ON,   KC_MUTE, KC_BTN1, KC_WH_D, KC_BTN2, KC_ACL0, KC_ENT,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
         )
 };
 
