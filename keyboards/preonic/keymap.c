1a984bb3d3ec1759d0a47e5579407bfb896737ed
Edward Kopp <edward@edwardkopp.com>
2022-12-16 22:49:46 -0500

    add transparent keys to colemak dhm layer

--- keyboards/preonic/keymap.c
+++ keyboards/preonic/keymap.c
@@ -27,11 +27,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, MO(2),   KC_SPC,  KC_SPC,  MO(3),   KC_RGUI, KC_RSFT, KC_RALT, KC_RCTL
         ),
     [1] = LAYOUT_preonic_grid(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MPLY,
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-        KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, MO(2),   KC_SPC,  KC_SPC,  MO(3),   KC_RGUI, KC_RSFT, KC_RALT, KC_RCTL
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        _______, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, _______,
+        _______, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    _______,
+        _______, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
         ),
     [2] = LAYOUT_preonic_grid(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
