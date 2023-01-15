bc6885a6a51aa77b02f937649f2851226bb885b1
Edward Kopp <edward@edwardkopp.com>
2023-01-14 20:14:51 -0500

    Update comment

--- keyboards/preonic/keymap.c
+++ keyboards/preonic/keymap.c
@@ -70,7 +70,7 @@ const key_override_t **key_overrides = (const key_override_t *[]) {
     NULL
 };
 
-// i sometimes think i have too many layers
+// the keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_preonic_grid(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MPLY,
