ca4448cb10880106da37660de290d40c1fe50ada
Edward Kopp <edward@edwardkopp.com>
2023-09-29 00:58:11 -0400

    improved keymap? maybe?

--- keyboards/preonic/keymap.c
+++ keyboards/preonic/keymap.c
@@ -21,81 +21,55 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 enum rainbow_layer_cake {
     _QWERTY,
     _SYMBOL,
-    _NAVI,
+    _FUN,
+    _NAV,
     _RAT
 };
 
 // layer keys
-#define EK_SYMB MO(_SYMBOL)
-#define EK_NAVI MO(_NAVI)
+#define EK_SYM MO(_SYMBOL)
+#define EK_NAV MO(_NAV)
+#define EK_RAT TG(_RAT)
 
-// KC_MPLY to KC_MUTE
-const key_override_t play_mute = ko_make_with_layers_and_negmods(
-    MOD_MASK_SHIFT,
-    KC_MPLY,
-    KC_MUTE,
-    ~0,
-    MOD_MASK_CAG
-);
-
-// KC_MNXT to KC_VOLU
-const key_override_t next_vold = ko_make_with_layers_and_negmods(
-    MOD_MASK_SHIFT,
-    KC_MNXT,
-    KC_VOLD,
-    ~0,
-    MOD_MASK_CAG
-);
-
-// KC_MPRV to KC_VOLD
-const key_override_t prev_volu = ko_make_with_layers_and_negmods(
-    MOD_MASK_SHIFT,
-    KC_MPRV,
-    KC_VOLU,
-    ~0,
-    MOD_MASK_CAG
-);
-
-// set all key overrides
-const key_override_t **key_overrides = (const key_override_t *[]) {
-    &play_mute,
-    &next_vold,
-    &prev_volu,
-    NULL
-};
-
-// the keymap
+// the keymap (using MIT Preonic layout)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_preonic_grid(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MPLY,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, EK_SYMB, KC_SPC,  KC_SPC,  EK_NAVI, KC_RSFT, KC_RALT, KC_RGUI, KC_RCTL
+        KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, EK_SYM,  KC_SPC,  KC_SPC,  EK_NAV,  XXXXXXX, KC_RALT, KC_RGUI, KC_RCTL
     ),
     [_SYMBOL] = LAYOUT_preonic_grid(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ENT,
         _______, KC_BSLS, KC_PIPE, KC_PLUS, KC_EQL,  KC_UNDS, KC_MINS, KC_LBRC, KC_LCBR, KC_RCBR, KC_RBRC, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
-    [_NAVI] = LAYOUT_preonic_grid(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-        KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_DEL,
-        KC_PAUS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX, KC_CAPS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_ENT,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
+    [_NAV] = LAYOUT_preonic_grid(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_INS,  XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_DEL,
+        EK_RAT,  KC_LSFT, KC_LALT, KC_LGUI, KC_LCTL, XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_ENT,
+        _______, XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+    [_FUN] = LAYOUT_preonic_grid(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        KC_PAUS, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   QK_BOOT,
+        EK_RAT,  KC_LSFT, KC_LALT, KC_LGUI, KC_LCTL, XXXXXXX, XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_CAPS,
+        _______, XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_RAT] = LAYOUT_preonic_grid(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
-        NK_OFF,  KC_HOME, KC_MS_U, KC_END,  KC_PGUP, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U, KC_BTN3, XXXXXXX, KC_CALC,
-        NK_ON,   KC_MS_L, KC_MS_D, KC_MS_R, KC_PGDN, XXXXXXX, KC_MPRV, KC_BTN1, KC_WH_D, KC_BTN2, KC_ACL0, KC_PSCR,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MNXT, KC_MPLY, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-        _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U, KC_PSCR, KC_BTN3, KC_PGUP, KC_HOME, KC_MS_U, KC_END,  KC_PGUP, KC_DEL,
+        KC_ESC,  KC_LSFT, KC_ALC0, KC_WH_D, KC_BTN1, KC_BTN2, KC_PGDN, KC_MS_L, KC_MS_D, KC_MS_R, KC_PGDN, KC_ENT,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN2, KC_BTN3, XXXXXXX, XXXXXXX, EK_RAT,
+        _______, _______, _______, _______, XXXXXXX, KC_BTN1, KC_BTN1, XXXXXXX, _______, _______, _______, _______
     )
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, _SYMBOL, _NAVI, _RAT);
+    return update_tri_layer_state(state, _SYMBOL, _NAV, _FUN);
 }
